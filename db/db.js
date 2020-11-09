6d99e126953b034748ea415f8e75f6354c0643db
harsh <54494999+harsh-singh-vst-au4@users.noreply.github.com>
2020-11-09 17:00:40 +0530

    bugfix, in db

--- db/db.js
+++ db/db.js
@@ -55,6 +55,10 @@ userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
 
+    delete userObject.password
+    delete userObject.tokens
+    delete userObject.avatar
+    
     return userObject
 }
 
