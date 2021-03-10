2e27c14613476dd4db3831beb0078cb3e9dda9b6
harsh.atdrive <54494999+harsh-singh-vst-au4@users.noreply.github.com>
2021-03-10 17:16:20 +0530

    changes to new laptop

--- db/db.js
+++ db/db.js
@@ -72,7 +72,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     const isMatch = await bcrypt.compare(password, user.password)
 
     if (!isMatch) {
-        throw new Error('Password Incorrect')
+        throw new Error('Password Incorrect') 
     }
 
     return user
